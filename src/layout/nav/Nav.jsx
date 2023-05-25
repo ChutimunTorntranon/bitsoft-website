2be7c78eb443ef61e25429064c0b99bca7c7648f
ChutimunTorntranon <ChutimunTorntranon@gmail.com>
2023-05-25 10:00:17 +0700

    responsiveNav

--- src/layout/nav/Nav.jsx
+++ src/layout/nav/Nav.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styleMain.css";
 function Nav() {
-  return <div className="Navbar"></div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">BitSoft Logo</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/service">Service</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
